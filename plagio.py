5ebdfb0ed03e61e85328635ea4c3d7a434b229db
xByForce <megajupa@hotmail.com>
2015-10-10 20:33:32 -0300

    The Program

---
+++ plagio.py
@@ -0,0 +1,29 @@
+from matplotlib.pylab import hist, show
+
+def his(linea):                 ##Funcion que retorna un dic que contiene la frecuencia de cada palabra en el parrafo
+    val = linea.strip().split()
+        dic = {}
+        for i in val:
+            if i not in dic:
+                dic[i] = val.count(i)
+        return dic
+
+def disteu():
+
+def discos():
+
+            
+
+a = raw_input("Ingrese nombre archivo 1 aqui: ")
+b = raw_input("Ingrese nombre archivo 2 aqui: ")
+
+archivo1 = open(a)
+for parrafo in archivo1:
+    if parrafo != "\n": ##Comprueba que el parrafo no sea solo un salto de linea
+        dic1 = his(parrafo)
+        archivo2 = open(b)
+        for parrafo2 in archivo2:
+            if parrafo2 != "\n": ##Comprueba que el parrafo no sea solo un salto de linea
+                dic2 = his(parrafo2)
+                
+        
