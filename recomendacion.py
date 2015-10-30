73a1fd577ae3fe4224bbb1044c9c09301251b356
vilu83 <vilu83@users.noreply.github.com>
2015-10-30 21:09:49 -0200

    Update recomendacion.py

--- recomendacion.py
+++ recomendacion.py
@@ -1,3 +1,9 @@
 #Juan Pablo San Martin
 #Patricio Campana
 #Tomas Cantuarias
++'''
++BX-Book-Ratings.csv --> "User-ID";"ISBN";"Book-Rating"
++BX-Users.csv -->"User-ID";"Location";"Age"
++BX-Books.csv -->"ISBN";"Book-Title";"Book-Author";"Year-Of-Publication";"Publisher";"Image-URL-S";"Image-URL-M";"Image-URL-L"
+'''
+#Dicc --> polio = {} --> polio[User-ID] = [(ISBN,Book-Rating),...
