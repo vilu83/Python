97f26a259337f11f576b0b7e5e016fc10e30fd40
vilu83 <vilu83@users.noreply.github.com>
2015-10-30 21:05:02 -0200

    Update recomendacion.py

--- recomendacion.py
+++ recomendacion.py
@@ -1,3 +1,7 @@
 #Juan Pablo San Martin
 #Patricio Campana
 #Tomas Cantuarias
+'''
+BX-Book-Ratings.csv --> "User-ID";"ISBN";"Book-Rating"
+BX-Users.csv -->"User-ID";"Location";"Age"
+BX-Books.csv -->"ISBN";"Book-Title";"Book-Author";"Year-Of-Publication";"Publisher";"Image-URL-S";"Image-URL-M";"Image-URL-L"
