0aaefff6020255e3d1cdae56df73f5c670eed731
xByForce <megajupa@hotmail.com>
2015-10-30 23:10:16 -0300

    Update recomendacion.py

--- recomendacion.py
+++ recomendacion.py
@@ -8,21 +8,19 @@
 
 #Dicc --> polio = {} --> polio[User-ID] = [(ISBN,Book-Rating),...
 
-def correlacion(usuario1, usurero):
-    libros1 = set()  #Conjunto vacio que va a contener los libros que ha calificado el usuario 1
-    libros2= set()   #Conjunto vacio que va a contener los libros que ha calificado el usuraio 2
-    usu1 = {}        #Diccionario del usuario 1 donde el libro es la llave y el rating el valor
-    usu2 = {}        #Diccionario del usuario 2 donde el libro es la llave y el rating el valor
-    for tupla in ratings[usuario1]:    #Diccionario donde el usuario es la llave y los valores son una lista de tuplas, con tuplas de la forma (Libro, Rating) 
+def LibrosUsuario(usuario):
+    for tupla in ratings[usuario]:    #Diccionario donde el usuario es la llave y los valores son una lista de tuplas, con tuplas de la forma (Libro, Rating) 
+        usu = {}                      #Diccionario del usuario 1 donde el libro es la llave y el rating el valor
+        libros = sset()               #Conjunto vacio que va a contener los libros que ha calificado el usuario
         libro, rating = tupla
-        if rating > 0:                 #Ignora los libros cuyos rating sean 0
-            libros1.add(libro)         #Agrega el libro al conjunto de los libros que ha calificado el usuario 1
-            usu1[libro] = rating       #Agrega el libro como llave y el rating como valor al diccionario del usuario 1
-    for tupla2 in ratings[usurero]:    #Diccionario donde el usuario es la llave y los valores son una lista de tuplas, con tuplas de la forma (Libro, Rating)
-        libro2, rating2 = tupla2
-        if rating2 > 0:                #Ignora los libros cuyos rating sean 0
-            libros2.add(libro2)        #Agrega el libro al conjunto de los libros que ha calificado el usuario 2
-            usu2[libro2]= rating2      #Agrega el libro como llave y el rating como valor al diccionario del usuario 1
+        if rating > 0:                #Ignora los libros cuyos rating sean 0
+            libros.add(libro)         #Agrega el libro al conjunto de los libros que ha calificado el usuario 
+            usu[libro] = rating       #Agrega el libro como llave y el rating como valor al diccionario del usuario    
+    return libros, usu
+
+def correlacion(usuario1, usurero):
+    libros1, usu1 = LibrosUsuario(usuario1)                   
+    libros2, usu2 = LibrosUsuario(usurero)
     MediaUsu1 = sum(usu1.values())/len(usu1.values())         #Media de los ratings del usuario 1, sin contar los ratings 0
     MediaUsu2 = sum(usu2.values())/len(usu2.values())         #Media de los ratings del usuario 2, sin contar los ratings 0
     InterLibros = libros1 & libros2                           #Conjunto producto de la interseccion de los libros que han leido el usuario 1 y el 2
@@ -34,7 +32,7 @@ def correlacion(usuario1, usurero):
         Sumatoria2 += (usu1[libro] - MediaUsu1)**2                                    #Se aplica la formula de correlacion
         Sumatoria3 += (usu2[libro] - MediaUsu2)**2                                    #Se aplica la formula de correlacion
     Correlacion = Sumatoria1 / ((Sumatoria2)**0.5 * (Sumatoria3)**0.5)                #Se aplica la formula de correlacion
-    return Correlacion                                                                #Retorna el resultado de la formula
+    return Correlacion                                                                #Retorna el resultado de la formula                         #Retorna el resultado de la formula
         
         
 
