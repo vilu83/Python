1a1d121a02e7bd96658c05d802a3340cc78c5dc7
xByForce <megajupa@hotmail.com>
2015-11-14 23:49:20 -0300

    Update imagenes.py

--- imagenes.py
+++ imagenes.py
@@ -1,3 +1,19 @@
+from PIL import Image
+from funciones import *
+import numpy as np
+import os
+
+def ArrayAImagen(arr, nombreSalida):
+    im = Image.fromarray(arr.clip(0,255).astype('uint8'), 'RGB')
+    im.save(nombreSalida)
+    return True
+
+def espejovertical(matriz):
+    espejo = np.array(matriz)[::-1]
+    salida = nombre+"espejovertical"+tipo
+    ArrayAImagen(espejo, salida)
+    os.system(salida)
+
 def  escaladeMrgrey (matriz)
 	for pixely in matriz:
 		for pixelx in matriz:
@@ -5,3 +21,21 @@ def  escaladeMrgrey (matriz)
 			gris = (red+green+blue)/3
 			valorRGBnew = [gris, gris, gris]
 			pixey[pixelx] = valorRGBnew
+
+def negativo(matriz):
+ for filaPix in matriz:
+  for pix in filaPix:
+   for color in range(3):
+    pix[color] = 255 - pix[color]
+
+nombre = raw_input("Ingrese el nombre de la imagen: ")
+tipo = raw_input("Ingrese el tipo de archivo de la imagen(inclyendo punto): ")
+imagen = nombre+tipo
+archivo = nombre + ".txt"
+convertirImagenAArchivo(imagen, archivo)
+matriz = leerArchivo(archivo)
+espejovertical(matriz)
+a = raw_input()
+
+
+
